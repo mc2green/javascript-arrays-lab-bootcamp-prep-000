4e6d22c62bab19afcff1875959ee3ac991bc475b
Matthew Chen <mc.2green@gmail.com>
2019-07-17 16:12:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-var kittens = ["Milo", "Otis", "Garfield"] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(name){
-  return kittens.push(name)
+  return kittens.push(name);
 }
 
 function destructivelyAppendKitten(name){
-  return kittens.unshift(name)
+  return kittens.unshift(name);
 }
 
 function destructivelyRemoveLastKitten(name){
-  
+  return kittens.pop(name);
 }
